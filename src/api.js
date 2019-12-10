0c1d25ba9154b0308288370b532b2fb3644f7325
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-12-10 13:51:17 +0300

    feat: add err response

--- src/api.js
+++ src/api.js
@@ -1,20 +1,32 @@
+const errResponse = {
+    links: [
+        {
+            url,
+            status: '???',
+            state: 'BROKEN'
+        }
+    ],
+    passed: false,
+};
+
 export async function crawl(url) {
     return new Promise((resolve, reject) => {
         console.log(`fetch ${url}...`);
         fetch(`/api/broken?url=${url}`)
-            .then(res => res.json())
+            .then(res => {
+                if (res && res.ok) {
+                    res.json()
+                } else {
+                    reject({
+                        ...errResponse,
+                    });
+                }
+            })
             .then(data => resolve(data))
             .catch(err => {
                 console.error(err);
                 reject({
-                    links: [
-                        {
-                            url,
-                            status: '???',
-                            state: 'BROKEN'
-                        }
-                    ],
-                    passed: false,
+                    ...errResponse,
                 });
             });
     });
