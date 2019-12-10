50c21194310eb38d08c42f0213aaa41cc0c4e449
Sergey Bondar <Bondar.S.Evg@sberbank.ru>
2019-12-10 13:37:26 +0300

    feat: server console in dev only

--- server/server.js
+++ server/server.js
@@ -4,6 +4,27 @@ const conf = require('dotenv').config();
 //const linkinator = require('linkinator');
 const linkinator = require('linkinator-css-edition');
 
+const IS_DEV = !!process.env.DEV;
+const IS_DISABLE_EXTERNAL = !!process.env.DISABLE_EXTERNAL;
+
+// create a new `LinkChecker` that we'll use to run the scan.
+const checker = new linkinator.LinkChecker();
+if (IS_DEV) {
+    // Respond to the beginning of a new page being scanned
+    checker.on('pagestart', url => {
+        console.log(`Scanning ${url}`);
+    });
+
+    // After a page is scanned, check out the results!
+    checker.on('link', res => {
+        if (res.state === 'SKIPPED') {
+            return;
+        }
+        // check the specific url that was scanned
+        console.log(`${res.state} ${res.status} ${res.url}`);
+    });
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,9 +33,6 @@ const config = require('./config');
 if (conf.error) {
     throw result.error;
 }
-
-const IS_DEV = !!process.env.DEV;
-const IS_DISABLE_EXTERNAL = !!process.env.DISABLE_EXTERNAL;
 
 const server = express();
 
@@ -34,23 +52,6 @@ server.get('/api/broken', async function (req, res) {
         }, 400);
     }
 
-    // create a new `LinkChecker` that we'll use to run the scan.
-    const checker = new linkinator.LinkChecker();
-
-    // Respond to the beginning of a new page being scanned
-    checker.on('pagestart', url => {
-        // console.log(`Scanning ${url}`);
-    });
-
-    // After a page is scanned, check out the results!
-    checker.on('link', res => {
-        if (res.state === 'SKIPPED') {
-            return;
-        }
-        // check the specific url that was scanned
-        // console.log(`${res.state} ${res.status} ${res.url}`);
-    });
-
     // Go ahead and start the scan! As events orccur, we will see them above.
     const result = await checker.check({
         path: req.query.url,
@@ -60,15 +61,17 @@ server.get('/api/broken', async function (req, res) {
         ...config,
     });
 
-    // Check to see if the scan passed!
-    console.log(result.passed ? 'PASSED :D' : 'FAILED :(');
+    if (IS_DEV) {
+        // Check to see if the scan passed!
+        console.log(result.passed ? 'PASSED :D' : 'FAILED :(');
 
-    // How many links did we scan?
-    console.log(`Scanned total of ${result.links.length} links!`);
+        // How many links did we scan?
+        console.log(`Scanned total of ${result.links.length} links!`);
 
-    // The final result will contain the list of checked links, and the pass/fail
-    const brokeLinksCount = result.links.filter(x => x.state === 'BROKEN');
-    console.log(`Detected ${brokeLinksCount.length} broken links.`);
+        // The final result will contain the list of checked links, and the pass/fail
+        const brokeLinksCount = result.links.filter(x => x.state === 'BROKEN');
+        console.log(`Detected ${brokeLinksCount.length} broken links.`);
+    }
 
     res.send(result);
 });
